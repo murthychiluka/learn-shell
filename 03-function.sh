978c5383c91941ff9827a7c06fe52174adc1f10e
Murthy <murthychiluka@gmail.com>
2023-02-18 19:33:06 +0530

    modified

--- 03-function.sh
+++ 03-function.sh
@@ -1,6 +1,10 @@
 #declaring a function
 xyz() {
     echo "hello from function"
+    echo 1st arguement $1
+    echo 2nd arguement $2
+    echo all arguements $*
+    echo no of arguements $#
 
 }
 # call a function
