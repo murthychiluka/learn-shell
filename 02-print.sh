f993999279b5dde2be4bf054c1418ad70e378664
Murthy <murthychiluka@gmail.com>
2023-02-22 11:34:28 +0530

    a

--- 02-print.sh
+++ 02-print.sh
@@ -12,3 +12,6 @@ echo -e "\e[COLm MESSAGE \e[0m"
 #Cyan    36
 #  \e[0m -> Zero code is going to reset the color, Meaning if we enable color for sure we need to disable it, otherwise the color will continue on screen
 echo -e "\e[31mHello in Red Color\e[0m"
+
+
+echo -e "\line1\nline2\nline3"
